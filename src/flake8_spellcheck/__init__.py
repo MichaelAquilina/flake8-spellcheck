2ec79e2d3c13bd4ec0623e52ab9751e7c0de602d
Michael Aquilina <michaelaquilina@gmail.com>
2018-09-10 22:44:40 +0100

    Tests and fixes

--- src/flake8_spellcheck/__init__.py
+++ src/flake8_spellcheck/__init__.py
@@ -6,49 +6,51 @@ import pkg_resources
 from string import ascii_lowercase, ascii_uppercase
 
 
-def split_class_def(name):
+def split_class_def(name, col_offset):
     buffer = ""
     for c in name:
-        if not buffer or c in ascii_lowercase:
+        if c in ascii_lowercase:
             buffer += c
         elif c in ascii_uppercase:
             if buffer:
-                yield buffer
-            buffer = ""
+                yield buffer, col_offset
+            buffer = c
 
 
-def split_name_def(name):
-    yield from name.split("_")
+def split_name_def(name, col_offset):
+    for token in name.split("_"):
+        yield token, col_offset
+        col_offset += len(token) + 1
+
 
 
 class SpellCheckPlugin(object):
     name = "flake8-spellcheck"
     version = "0.1.0"
-    wadddd = "d"
 
     def __init__(self, tree, *args, **kwargs):
         self.tree = tree
 
         # TODO: this can definitely be simplified
         with open(pkg_resources.resource_filename(__name__, "words.txt"), "r") as fp:
-           data = fp.read()
+            data = fp.read()
 
         self.words = set(w.lower() for w in data.split("\n"))
 
     def run(self):
         for node in ast.walk(self.tree):
             if isinstance(node, ast.ClassDef):
-                tokens = split_class_def(node.name)
+                tokens = split_class_def(node.name, node.col_offset)
             elif isinstance(node, ast.Name):
-                tokens = split_name_def(node.id)
+                tokens = split_name_def(node.id, node.col_offset)
             else:
                 continue
 
-            for t in tokens:
+            for t, col_offset in tokens:
                 if t.lower() not in self.words:
                     yield (
                         node.lineno,
-                        node.col_offset,
-                        "SP1 Unknown word: {}".format(t),
+                        col_offset,
+                        "SP1 Unknown word: '{}'".format(t),
                         type(self),
                     )
--- tests/test_flake8_spellcheck.py
+++ tests/test_flake8_spellcheck.py
@@ -1,9 +1,41 @@
 # -*- coding: utf-8 -*-
 
-def test_class_name_pass(flake8dir):
-    flake8dir.make_example_py("""
-        class FakeClassName:
-            pass
-    """)
-    result = flake8dir.run_flake8()
-    assert result.out_lines == []
+
+class TestName:
+    def test_fail(self, flake8dir):
+        flake8dir.make_example_py("""
+            my_varaible_namde = "something"
+        """)
+        result = flake8dir.run_flake8()
+        assert result.out_lines == [
+            "./example.py:1:4: SP1 Unknown word: 'varaible'",
+            "./example.py:1:13: SP1 Unknown word: 'namde'",
+        ]
+
+    def test_pass(self, flake8dir):
+        flake8dir.make_example_py("""
+            my_variable_name = "something"
+        """)
+        result = flake8dir.run_flake8()
+        assert result.out_lines == []
+
+
+class TestClassDef:
+    def test_fail(self, flake8dir):
+        flake8dir.make_example_py("""
+            class FackeClaessName:
+                pass
+        """)
+        result = flake8dir.run_flake8()
+        assert result.out_lines == [
+            "./example.py:1:1: SP1 Unknown word: 'Facke'",
+            "./example.py:1:1: SP1 Unknown word: 'Claess'",
+        ]
+
+    def test_pass(self, flake8dir):
+        flake8dir.make_example_py("""
+            class FakeClassName:
+                pass
+        """)
+        result = flake8dir.run_flake8()
+        assert result.out_lines == []
