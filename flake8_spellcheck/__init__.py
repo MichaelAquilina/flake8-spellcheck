803ecf33bf00dac1696a9e318fa934e5968a7fc8
Michael Aquilina <michaelaquilina@gmail.com>
2018-09-12 23:36:13 +0100

    Read version directly from spellcheck plugin

--- flake8_spellcheck/__init__.py
+++ flake8_spellcheck/__init__.py
@@ -34,7 +34,7 @@ def parse_snake_case(name, col_offset):
 
 class SpellCheckPlugin(object):
     name = "flake8-spellcheck"
-    version = "0.2.3"
+    version = "0.2.4"
 
     def __init__(self, tree, *args, **kwargs):
         self.tree = tree
--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 import setuptools
 
+from flake8_spellcheck import SpellCheckPlugin
+
 requires = ["flake8 > 3.0.0"]
 
 setuptools.setup(
     name="flake8-spellcheck",
     license="MIT",
-    version="0.2.3",
+    version=SpellCheckPlugin.version,
     description="Spellcheck variables, comments and docstrings",
     author="Michael Aquilina",
     author_email="michaelaquilina@gmail.com",
