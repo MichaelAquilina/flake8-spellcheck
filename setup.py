78089da804ae03deffc6ec8a559219cc03b10759
Michael Aquilina <michaelaquilina@gmail.com>
2018-09-18 23:40:53 +0100

    Bump to version 0.4.6

--- setup.py
+++ setup.py
@@ -3,11 +3,15 @@ from flake8_spellcheck import SpellCheckPlugin
 
 requires = ["flake8 > 3.0.0"]
 
+with open("README.rst") as fp:
+    long_description = fp.read()
+
 setuptools.setup(
     name="flake8-spellcheck",
     license="MIT",
     version=SpellCheckPlugin.version,
     description="Spellcheck variables, comments and docstrings",
+    long_description=long_description,
     author="Michael Aquilina",
     author_email="michaelaquilina@gmail.com",
     url="https://github.com/MichaelAquilina/flake8-spellcheck",
