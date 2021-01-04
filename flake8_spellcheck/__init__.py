584fcc7df4c56e3bbe446a952519dae1c87e7795
Paulo V C Medeiros <paulo.medeiros@smhi.se>
2021-01-04 13:54:04 +0100

    Fix lint

--- flake8_spellcheck/__init__.py
+++ flake8_spellcheck/__init__.py
@@ -169,7 +169,7 @@ class SpellCheckPlugin:
             and "comments" in self.spellcheck_targets
             # Ensure comment is neither empty nor a sequence of "#" characters
             # github.com/MichaelAquilina/flake8-spellcheck/issues/34
-            and token_info.string.lstrip("#").strip() != ''
+            and token_info.string.lstrip("#").strip() != ""
             # Ignore flake8 pragma comments
             and token_info.string.lstrip("#").split()[0] != "noqa:"
         )
